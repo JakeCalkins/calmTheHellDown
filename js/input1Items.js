9e4c5c573befb2a6b3024d93d5e88d05b9ccb5fa
Brian Dang <bldang@umass.edu>
2017-11-04 13:16:32 -0400

    Made changes to HTML files

--- js/input1Items.js
+++ js/input1Items.js
@@ -1,7 +1,9 @@
 
-$(document).ready(function() {
+$(document).ready(
+  function() {
   var country = ["Australia", "Bangladesh", "Denmark", "Hong Kong", "Indonesia", "Netherlands", "New Zealand", "South Africa"];
   $("#country").select2({
     data: country
   });
+  
 });
