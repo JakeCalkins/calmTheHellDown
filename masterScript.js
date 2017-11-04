9e6363b27ec3f09e6adbf1469963aff71f2583a8
Nick <njgeor16@gmail.com>
2017-11-04 18:00:02 -0400

    added user input boxes and functionality

--- masterScript.js
+++ masterScript.js
@@ -1,9 +1,3 @@
-
-
-
-
-
-
 function itemOneNameStorage(){
 	document.getElementById("app").value = applianceName1.options[applianceName1.selectedIndex].text;
 	var displayItemOne = applianceName1.options[applianceName1.selectedIndex].text;
@@ -42,4 +36,114 @@ function itemFourNameStorage(){
 function itemFourLocationStorage(){
 	document.getElementById("app").value = applianceLocation4.options[applianceLocation4.selectedIndex].text;
 	var displayLocationFour = applianceLocation4.options[applianceLocation4.selectedIndex].text;
+}
+
+function itemFiveNameStorage(){
+	document.getElementById("app").value = applianceName5.options[applianceName5.selectedIndex].text;
+	var displayItemFive = applianceName5.options[applianceName5.selectedIndex].text;
+}
+
+function itemFiveLocationStorage(){
+	document.getElementById("app").value = applianceLocation5.options[applianceLocation5.selectedIndex].text;
+	var displayLocationFive = applianceLocation5.options[applianceLocation5.selectedIndex].text;
+}
+
+function itemSixNameStorage(){
+	document.getElementById("app").value = applianceName6.options[applianceName6.selectedIndex].text;
+	var displayItemSix = applianceName6.options[applianceName6.selectedIndex].text;
+}
+
+function itemSixLocationStorage(){
+	document.getElementById("app").value = applianceLocation6.options[applianceLocation6.selectedIndex].text;
+	var displayLocationSix = applianceLocation6.options[applianceLocation6.selectedIndex].text;
+}
+
+function itemSevenNameStorage(){
+	document.getElementById("app").value = applianceName7.options[applianceName7.selectedIndex].text;
+	var displayItemSeven = applianceName7.options[applianceName7.selectedIndex].text;
+}
+
+function itemSevenLocationStorage(){
+	document.getElementById("app").value = applianceLocation7.options[applianceLocation7.selectedIndex].text;
+	var displayLocationSeven = applianceLocation7.options[applianceLocation7.selectedIndex].text;
+}
+
+function itemEightNameStorage(){
+	document.getElementById("app").value = applianceName8.options[applianceName8.selectedIndex].text;
+	var displayItemEight = applianceName8.options[applianceName8.selectedIndex].text;
+}
+
+function itemEightLocationStorage(){
+	document.getElementById("app").value = applianceLocation8.options[applianceLocation8.selectedIndex].text;
+	var displayLocationEight = applianceLocation8.options[applianceLocation8.selectedIndex].text;
+}
+
+function itemNineNameStorage(){
+	document.getElementById("app").value = applianceName9.options[applianceName9.selectedIndex].text;
+	var displayItemNine = applianceName9.options[applianceName9.selectedIndex].text;
+}
+
+function itemNineLocationStorage(){
+	document.getElementById("app").value = applianceLocation9.options[applianceLocation9.selectedIndex].text;
+	var displayLocationNine = applianceLocation9.options[applianceLocation9.selectedIndex].text;
+}
+
+function itemTenNameStorage(){
+	document.getElementById("app").value = applianceName10.options[applianceName10.selectedIndex].text;
+	var displayItemTen = applianceName10.options[applianceName10.selectedIndex].text;
+}
+
+function itemTenLocationStorage(){
+	document.getElementById("app").value = applianceLocation10.options[applianceLocation10.selectedIndex].text;
+	var displayLocationTen = applianceLocation10.options[applianceLocation10.selectedIndex].text;
+}
+
+function itemElevenNameStorage(){
+	document.getElementById("app").value = applianceName11.options[applianceName11.selectedIndex].text;
+	var displayItemEleven = applianceName11.options[applianceName11.selectedIndex].text;
+}
+
+function itemElevenLocationStorage(){
+	document.getElementById("app").value = applianceLocation11.options[applianceLocation11.selectedIndex].text;
+	var displayLocationEleven = applianceLocation11.options[applianceLocation11.selectedIndex].text;
+}
+
+function itemTwelveNameStorage(){
+	document.getElementById("app").value = applianceName12.options[applianceName12.selectedIndex].text;
+	var displayItemTwelve = applianceName12.options[applianceName12.selectedIndex].text;
+}
+
+function itemTwelveLocationStorage(){
+	document.getElementById("app").value = applianceLocation12.options[applianceLocation12.selectedIndex].text;
+	var displayLocationTwelve = applianceLocation12.options[applianceLocation12.selectedIndex].text;
+}
+
+function itemThirteenNameStorage(){
+	document.getElementById("app").value = applianceName13.options[applianceName13.selectedIndex].text;
+	var displayItemThirteen = applianceName13.options[applianceName13.selectedIndex].text;
+}
+
+function itemThirteenLocationStorage(){
+	document.getElementById("app").value = applianceLocation13.options[applianceLocation13.selectedIndex].text;
+	var displayLocationThirteen = applianceLocation13.options[applianceLocation13.selectedIndex].text;
+}
+
+function itemFourteenNameStorage(){
+	document.getElementById("app").value = applianceName14.options[applianceName14.selectedIndex].text;
+	var displayItemFourteen = applianceName14.options[applianceName14.selectedIndex].text;
+}
+
+function itemFourteenLocationStorage(){
+	document.getElementById("app").value = applianceLocation14.options[applianceLocation14.selectedIndex].text;
+	var displayLocationFourteen = applianceLocation14.options[applianceLocation14.selectedIndex].text;
+}
+
+function itemFifteenNameStorage(){
+	document.getElementById("app").value = applianceName15.options[applianceName15.selectedIndex].text;
+	var displayItemFifteen = applianceName15.options[applianceName15.selectedIndex].text;
+}
+
+function itemFifteenLocationStorage(){
+	document.getElementById("app").value = applianceLocation15.options[applianceLocation15.selectedIndex].text;
+	var displayLocationFifteen = applianceLocation15.options[applianceLocation15.selectedIndex].text;
 }
