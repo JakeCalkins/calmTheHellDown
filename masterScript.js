ecffa4b3c771fec7ecef5b7c36d47be0ccda2fc7
Nick <njgeor16@gmail.com>
2017-11-04 17:04:36 -0400

    javascript implementation

--- masterScript.js
+++ masterScript.js
@@ -1,5 +1,45 @@
-function functionA(){
-	var e = document.getElementById("appliance");
-	document.getElementById("app").value = appliance.options[appliance.selectedIndex].text;
-	
+
+
+
+
+
+
+function itemOneNameStorage(){
+	document.getElementById("app").value = applianceName1.options[applianceName1.selectedIndex].text;
+	var displayItemOne = applianceName1.options[applianceName1.selectedIndex].text;
+}
+
+function itemOneLocationStorage(){
+	document.getElementById("app").value = applianceLocation.options[applianceLocation.selectedIndex].text;
+	var displayLocationOne = applianceLocation.options[applianceLocation.selectedIndex].text;
+}
+
+function itemTwoNameStorage(){
+	document.getElementById("app").value = applianceName2.options[applianceName2.selectedIndex].text;
+	var displayItemTwo = applianceName2.options[applianceName2.selectedIndex].text;
+}
+
+function itemTwoLocationStorage(){
+	document.getElementById("app").value = applianceLocation2.options[applianceLocation2.selectedIndex].text;
+	var displayLocationTwo = applianceLocation2.options[applianceLocation2.selectedIndex].text;
+}
+
+function itemThreeNameStorage(){
+	document.getElementById("app").value = applianceName3.options[applianceName3.selectedIndex].text;
+	var displayItemThree = applianceName3.options[applianceName3.selectedIndex].text;
+}
+
+function itemThreeLocationStorage(){
+	document.getElementById("app").value = applianceLocation3.options[applianceLocation3.selectedIndex].text;
+	var displayLocationThree = applianceLocation3.options[applianceLocation3.selectedIndex].text;
+}
+
+function itemFourNameStorage(){
+	document.getElementById("app").value = applianceName4.options[applianceName4.selectedIndex].text;
+	var displayItemFour = applianceName4.options[applianceName4.selectedIndex].text;
+}
+
+function itemFourLocationStorage(){
+	document.getElementById("app").value = applianceLocation4.options[applianceLocation4.selectedIndex].text;
+	var displayLocationFour = applianceLocation4.options[applianceLocation4.selectedIndex].text;
 }
