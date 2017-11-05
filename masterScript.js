058a7002e18f0e35786d044143ab667b863cfd7c
Brian Dang <bldang@umass.edu>
2017-11-05 01:16:45 -0400

    Fix

--- masterScript.js
+++ masterScript.js
@@ -16,22 +16,6 @@ var displayItemThirteen;
 var displayItemFourteen;
 var displayItemFifteen;
 
-var kwhOne;
-var kwhTwo;
-var kwhThree;
-var kwhFour;
-var kwhFive;
-var kwhSix;
-var kwhSeven;
-var kwhEight;
-var kwhNine;
-var kwhTen;
-var kwhEleven;
-var kwhTwelve;
-var kwhThirteen;
-var kwhFourteen;
-var kwhFifteen;
-
 var displayLocationOne;
 var displayLocationTwo;
 var displayLocationThree;
@@ -52,17 +36,9 @@ var nameArr = ["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14",
 var locArr = ["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19"];
 
 var bedHour;
-<<<<<<< HEAD
-var offHour;
-=======
 var officeHour;
->>>>>>> 634b830fc38b3d957643df58738578567c681f65
 var livHour;
 var kitHour;
-
-var timeResult;
-var regionFactor;
-var regionString;
 
 function itemOneNameStorage(){
 	document.getElementById("app").value = applianceName1.options[applianceName1.selectedIndex].text;
@@ -245,6 +221,7 @@ function itemFifteenLocationStorage(){
 
 function onAddButtonClick(){
 	applianceCounter++;
+	console.log(applianceCounter);
 
 	if(applianceCounter == 6){
 		document.getElementById("applianceForm6").style.display = "block";
@@ -279,113 +256,7 @@ function onAddButtonClick(){
 	}
 }
 
-<<<<<<< HEAD
-function onContinueButtonClick(){
-	kwhOne = document.getElementById("item1Power");
-	kwhTwo = document.getElementById("item2Power");
-	kwhThree = document.getElementById("item3Power");
-	kwhFour = document.getElementById("item4Power");
-	kwhFive = document.getElementById("item5Power");
-	if(applianceCounter > 5){
-		kwhSix = document.getElementById("item6Power");
-	}
-	if(applianceCounter > 6){
-		kwhSeven = document.getElementById("item7Power");
-	}
-	if(applianceCounter > 7){
-		kwhEight = document.getElementById("item8Power");
-	}
-	if(applianceCounter > 8){
-		kwhNine = document.getElementById("item9Power");
-	}
-	if(applianceCounter > 9){
-		kwhTen = document.getElementById("item10Power");
-	}
-	if(applianceCounter > 10){
-		kwhEleven = document.getElementById("item11Power");
-	}
-	if(applianceCounter > 11){
-		kwhTwelve = document.getElementById("item12Power");
-	}
-	if(applianceCounter > 12){
-		kwhThirteen= document.getElementById("item13Power");
-	}
-	if(applianceCounter > 13){
-		kwhFourteen = document.getElementById("item14Power");
-	}
-	if(applianceCounter > 14){
-		kwhFifteen = document.getElementById("item15Power");
-	}
-}
 
-function calculateEnergyConsumption(){
-	var totalEnergy;
-
-	/*totalEnergy = totalEnergy + ((item1Power * timeAssignment(displayLocationOne)) / 1000)
-	totalEnergy = totalEnergy + ((item2Power * timeAssignment(displayLocationTwo)) / 1000)
-	totalEnergy = totalEnergy + ((item3Power * timeAssignment(displayLocationThree)) / 1000)
-	totalEnergy = totalEnergy + ((item4Power * timeAssignment(displayLocationFour)) / 1000)
-	totalEnergy = totalEnergy + ((item5Power * timeAssignment(displayLocationFive)) / 1000)
-	if(applianceCounter > 5){
-		totalEnergy = totalEnergy + ((item6Power * timeAssignment(displayLocationSix)) / 1000)	
-	}
-	if(applianceCounter > 6){
-		totalEnergy = totalEnergy + ((item7Power * timeAssignment(displayLocationSeven)) / 1000)	
-	}
-	if(applianceCounter > 7){
-		totalEnergy = totalEnergy + ((item8Power * timeAssignment(displayLocationEight)) / 1000)	
-	}
-	if(applianceCounter > 8){
-		totalEnergy = totalEnergy + ((item9Power * timeAssignment(displayLocationNine)) / 1000)	
-	}
-	if(applianceCounter > 9){
-		totalEnergy = totalEnergy + ((item10Power * timeAssignment(displayLocationTen)) / 1000)	
-	}
-	if(applianceCounter > 10){
-		totalEnergy = totalEnergy + ((item11Power * timeAssignment(displayLocationEleven)) / 1000)	
-	}
-	if(applianceCounter > 11){
-		totalEnergy = totalEnergy + ((item12Power * timeAssignment(displayLocationTwelve)) / 1000)	
-	}
-	if(applianceCounter > 12){
-		totalEnergy = totalEnergy + ((item13Power * timeAssignment(displayLocationThirteen)) / 1000)	
-	}
-	if(applianceCounter > 13){
-		totalEnergy = totalEnergy + ((item14Power * timeAssignment(displayLocationFourteen)) / 1000)	
-	}
-	if(applianceCounter > 14){
-		totalEnergy = totalEnergy + ((item15Power * timeAssignment(displayLocationFifteen)) / 1000)	
-	}*/
-
-	document.getElementById("app").value = region.options[region.selectedIndex].value;
-	regionString = region.options[region.selectedIndex].value;
-
-	console.log(regionString);
-	
-	if(regionString.localeCompare("NE") == 0) regionFactor = 121.60;
-	if(regionString.localeCompare("MA") == 0) regionFactor = 112.81;
-	if(regionString.localeCompare("ENC") == 0) regionFactor = 99.87;
-	if(regionString.localeCompare("WNC") == 0) regionFactor = 104.61;
-	if(regionString.localeCompare("SA") == 0) regionFactor = 131.20;
-	if(regionString.localeCompare("ESC") == 0) regionFactor = 130.31;
-	if(regionString.localeCompare("WSC") == 0) regionFactor = 128.81;
-	if(regionString.localeCompare("M") == 0) regionFactor = 99.91;
-
-	console.log(regionFactor);
-	var cout = document.getElementById("stateEnergy");
-	cout.innerHTML = regionFactor;
-
-	}
-
-function timeAssignment(location){
-	if(location.localeCompare("Living Room") == 0) timeResult = livHour;
-	if(location.localeCompare("Office") == 0) timeResult = offHour;
-	if(location.localeCompare("Kitchen") == 0) timeResult = kitHour;
-	if(location.localeCompare("Bedroom") == 0) timeResult = bedHour;
-}
-=======
-
->>>>>>> 634b830fc38b3d957643df58738578567c681f65
 
 function storeBedHour(num){
 	bedHour = num;
@@ -401,7 +272,7 @@ function storeKitHour(num){
 }
 
 function recommendation(){
-	
+
 	var CompOut = document.getElementById("CompRecommend");
 	var TVOut = document.getElementById("TVRecommend");
 	var OvenOut = document.getElementById("OvenRecommend");
